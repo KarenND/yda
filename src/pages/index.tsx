9157d457b32786bfa8a285b0ef569bfaf47f4696
KarenND <ninodiazkp.7@gmail.com>
2024-10-20 15:15:51 -0600

    Cambios 20-oct

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,178 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const car: any = {
+    year: 2012,
+    brand: "Chevrolet",
+    model: "Impala",
+    mileage: 127463,
+    location: "Tavares",
+    extendedLocation: "Tavares, FL (200 E. Burleigh Blvd)",
+    engine: "3.6L V6 DOHC 16V FFV",
+    transmision: "6-Speed Automatic",
+    vin: "2G1WC5E36C1158443",
+    color: "gold",
+    image: "https://cdn.prod.website-files.com/659d52e325ba52d02c9bab63/670f4c2509ee683b20d2e237_4e0f67ad-d923-4c68-a073-f96da1cb7a40.jpeg",
+    safety: [
+        "ABS Brakes",
+        "Child Safety Door Locks",
+        "Driver Airbag",
+        "Fog Lights",
+        "Front Side Airbag",
+        "Keyless Entry",
+        "Passenger Airbag",
+        "Rear Window Defogger",
+        "Traction Control",
+        "Vehicle Anti-Theft",
+        "Vehicle Stability Control System",
+    ],
+    comfort: [
+        "Air Conditioning",
+        "AM/FM Radio",
+        "Cruise Control",
+        "Driver Multi-Adjustable Power Seat",
+        "Front Heated Seat",
+        "Manual Sunroof",
+        "Passenger Multi-Adjustable Power Seat",
+        "Power Windows",
+        "Remote Ignition",
+        "Separate Driver/Front Passenger Climate Controls",
+        "Tilt Steering",
+        "Tilt Steering Column",
+    ],
+    exterior: [
+        "Alloy Wheels",
+        "Automatic Headlights",
+        "Daytime Running Lights",
+        "Front Air Dam",
+        "Heated Exterior Mirror",
+        "Rear Spoiler",
+        "Splash Guards",
+    ],
+    interior: [
+        "Cargo Net",
+        "Electrochromic Interior Rearview Mirror",
+        "Leather Seat",
+        "Leather Steering Wheel",
+        "Second Row Folding Seat",
+        "Steering Wheel Mounted Controls",
+        "Trip Computer",
+    ],
+    entertainment: [
+        "CD Player",
+        "Voice Activated Telephone",
+    ],
+    specs: [
+        "Tachometer",
+        "Telematics System",
+        "Tire Pressure Monitor",
+        "Trunk Anti-Trap Device",
+    ],
+    others: [
+        "Interval Wipers",
+        "Power Adjustable Exterior Mirror",
+        "Power Door Locks",
+        "Power Sunroof",
+        "Side Head Curtain Airbag",
+    ],
+
+  } 
+
+
+  return (
+    <div>
+      <div className="d-flex">
+      <div>
+          <img className="car-image" src={car.image} alt="Car Image" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="vehicle-information">
+          <span>
+            <h2>{car.year} / {car.brand} / {car.model}</h2>
+          </span>
+          <div>
+          <h4>Milleage: <span className="weight-normal">{car.mileage}</span> <br/> Location: <span className="weight-normal">{car.location}</span></h4>  
+          <div>
+            <h4 className="text-red">This car is on sale!*</h4>
+            <button className="cta">Get Pre-Approved</button>
+            <p className="terms">Complete our no-commitment <br/> pre-approval process in 3 minutes! </p>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>General Information</h2>
+        <h4>Engine: <span className="weight-normal">{car.engine}</span> <br/> 
+        Transmision: <span className="weight-normal">{car.transmision}</span> <br/> 
+        VIN: <span className="weight-normal">{car.vin}</span> <br/> 
+        Extended location: <span className="weight-normal">{car.extendedLocation}</span><br/> 
+        Color: <span className="weight-normal">{car.color}</span></h4>
+      </div>
+      <div>
+        <h3>Safety:</h3>
+        <br/>
+        <ul>
+          {
+            car.safety.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>Comfort:</h3> 
+        <br/>
+        <ul>
+          {
+            car.comfort.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>Exterior:</h3>
+        <br/>
+        {
+            car.exterior.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+      </div>
+      <div>
+        <h3>Interior:</h3> 
+        <br/>
+        {
+            car.interior.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+      </div>
+      <div>
+        <h3>Entertainment:</h3> 
+        <br/>
+        {
+            car.entertainment.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+        </div>
+      <div>
+        <h3>Specs:</h3>
+        <br/>
+        {
+            car.specs.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+      </div>
+      <div>
+        <h3>Others:</h3>
+        <br/>
+        {
+            car.others.map((item:string) => (
+              <li>{item}</li>
+            ))
+          }
+      </div>
     </div>
   );
 }
